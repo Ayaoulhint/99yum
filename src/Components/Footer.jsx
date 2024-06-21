9fa578caf3ab67002f9d0bbccf30326ba3662b55
ayaoulhint <oulhintaya702@gmail.com>
2024-06-21 03:20:01 +0100

    bug-fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,6 +8,7 @@ import pic1 from '../assets/foot1.png';
 function Footer() {
   return (
     <>
+    <div>
       <div className=' text-black flex items-center  justify-center'>
         <div className='flex flex-col gap-2 text-center'>
           <h1 className='text-2xl'>Navigation</h1>
