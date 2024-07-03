24580b3b5ab7bb5d318ee773c4282438f1771804
Mohd Arshan <110154295+arshansaifi150@users.noreply.github.com>
2024-07-03 06:57:18 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -43,7 +43,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
                 "anchor",
             ],
             toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            "undo redo | fontsize | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
         }}
         onEditorChange={onChange}
