444ae69491a18c49886a6ef34213b9f0304c1f1b
Mohd Arshan <arshansaifi150@gmail.com>
2024-07-04 01:14:26 +0530

    contact update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,12 @@
-import React,{useRef} from 'react';
+import React,{useRef,useState} from 'react';
 import largeImage from '../assets/contact.jpg'; 
 import emailjs from '@emailjs/browser'
 
 const ContactUs = () => {
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -15,12 +18,19 @@ const ContactUs = () => {
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          return (
+            alert('Message Sent')
+          )
+          
         },
         (error) => {
           console.log('FAILED...', error.text);
         },
-      );
+      )
+      setName('')
+      setEmail('')
+      setMessage('')
+
       
   };
 
@@ -35,13 +45,23 @@ const ContactUs = () => {
         <p className="md:text-lg mb-3 mt-3 text-md">Have a question, suggestion, or just want to say hello? We'd love to hear from you! Feel free to reach out to us using the form below, and we'll get back to you as soon as possible.</p>
         <form className="flex flex-col" ref={form} onSubmit={sendEmail}>
           <label htmlFor="name" className="md:text-lg text-md font-medium mb-2">Name</label>
-          <input type="text" id="name" name="user_name" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500"/>
+          <input type="text" id="name" name="user_name" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500" 
+          value={name}
+          onChange={(e) => setName(e.target.value)} required 
+          />
 
           <label htmlFor="email" className="md:text-lg text-md font-medium mb-2">Email</label>
-          <input type="email" id="email" name="user_email" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500"/>
+          <input type="email" id="email" name="user_email" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} required
+          />
 
           <label htmlFor="message" className="md:text-lg text-md font-medium mb-2">Message</label>
-          <textarea id="message" name="message" rows="4" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500"></textarea>
+          <textarea id="message" name="message" rows="4" className="py-2 px-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-orange-500"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)} required
+
+          ></textarea>
 
           <button type="submit" value="send" className="bg-gradient-to-r from-orange-500 to-red-500 py-2 px-4 text-white rounded-md shadow-lg hover:from-red-500 hover:to-orange-500 transition-colors duration-300 mb-10">Send</button>
         </form>
